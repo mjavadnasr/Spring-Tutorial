fe90db2689c3b16ac6fc1eb705ba4ebb317b91d1
mjavadnasr <m.javad.nasrolla@gmail.com>
2022-03-21 18:34:37 +0330

    def of @Qualifier

--- src/main/java/ConfigurationBeanFile/MediaTek.java
+++ src/main/java/ConfigurationBeanFile/MediaTek.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
-public class Mediatek implements MobileProcessor {
+
+public class MediaTek implements MobileProcessor {
     @Override
     public void process() {
         System.out.println("cpu is Mediatek");
--- src/main/java/ConfigurationBeanFile/Samsung.java
+++ src/main/java/ConfigurationBeanFile/Samsung.java
@@ -1,12 +1,15 @@
 package ConfigurationBeanFile;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Samsung {
 
     @Autowired
+    @Qualifier("mediaTek")
+
     MobileProcessor cpu ;
     public void config()
     {
