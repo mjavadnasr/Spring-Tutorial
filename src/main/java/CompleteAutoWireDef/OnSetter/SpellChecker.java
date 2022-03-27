2371d8e9b5440d26363ee09adca7129ac6d2319e
mjavadnasr <m.javad.nasrolla@gmail.com>
2022-03-28 01:39:45 +0430

    AutoWire On Setter

---
+++ src/main/java/CompleteAutoWireDef/OnSetter/SpellChecker.java
@@ -0,0 +1,13 @@
+package CompleteAutoWireDef.OnSetter;
+
+public class SpellChecker {
+    public SpellChecker()
+    {
+        System.out.println("inside SpellChecker Constructor");
+    }
+
+    public void checkSpelling()
+    {
+        System.out.println("Inside SpellChecking");
+    }
+}
---
+++ src/main/java/CompleteAutoWireDef/OnSetter/TextEditor.java
@@ -0,0 +1,22 @@
+package CompleteAutoWireDef.OnSetter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TextEditor {
+    private SpellChecker spellChecker;
+
+    @Autowired
+    public void setSpellChecker(SpellChecker spellChecker) {
+        this.spellChecker = spellChecker;
+    }
+
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+
+
+    public void SpellCheck() {
+
+        spellChecker.checkSpelling();
+    }
+}
